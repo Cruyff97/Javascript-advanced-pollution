67738a3e74e1684745e4f205d85ae356d13d86c1
Matteo Pellegrini <pelmat97@gmail.com>
2021-10-25 23:22:22 +0200

    main

--- src/js/index.js
+++ src/js/index.js
@@ -162,7 +162,7 @@ window.onload = function () {
     const json3 = await response.json();
     const cityqual = document.getElementById("cityquality");
     cityqual.textContent = "";
-    console.log(cityqual);
+    console.log(city_url);
     const backgroundRes = document.querySelector(".result");
     const citySearchvoteh3 = document.querySelector("#citySearchvoteh3");
     const citySearchDiv = document.querySelector("#citySearchDiv");
